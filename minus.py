dd358e9bc6b4cd15f4fa1cebd72d4f53bbd8721f
Simone Busoli <simone.busoli@gmail.com>
2024-11-06 12:07:16 +0000

    chore: minus description

--- minus.py
+++ minus.py
@@ -4,9 +4,10 @@ import pandas as pd
 
 with st.form("minus"):
     st.subheader("Compensate minus")
+    st.write('This form allows you to calculate how much plus you must generate to compensate a certain amount of minus.')
     
     minusAmount = st.number_input("Minus amount", value=1000)
-    capitalTax = st.number_input("Capital gain tax % of the asset you want to sell", value=26)
+    capitalTax = st.number_input("Capital gain tax % of the asset you want to sell", value=26.0)
     profitFromStock = st.number_input('Total profit from asset you want to sell', value=None)
     quotes = st.number_input('Number of quotes you own of the asset you want to sell', value=None)
 
